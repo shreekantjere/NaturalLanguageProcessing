863cbec967c52ef5049e675d21d2424dd74e3e5e
shreekantjere <shrikantjere@gmail.com>
2018-03-11 14:47:53 +0530

    added comments

--- spell_suggestion_words_levenshtein.py
+++ spell_suggestion_words_levenshtein.py
@@ -1,26 +1,29 @@
-import distance
-import nltk
+######Below code gives suggestion in a spell checker############
 
+import nltk
+import distance
+
+#Array dict_words stores the words in english dictionary
 dict_words=nltk.corpus.words.words('en')
 
-#print(dict_words)
+#Array to store similar words
 sim_words = []
+
+#Array to store similarity values for each word in sim_words array
 sim_dist = []
+
 
 for word in dict_words:
     if distance.nlevenshtein(word, "applicablity", method=1) <= 0.25 :
         sim_words.append(word)
         sim_dist.append(distance.nlevenshtein(word, "applicablity", method=1))
-        #print(word)
 
 
-print(sim_words)
-print(sim_dist)
+#Minimum value among all the sim_dist values, which represents more similarity#
 min_value = min(sim_dist)
-print(min_value)
 
+#Extracting the index of the min_value
 index_min_val= sim_dist.index(min(sim_dist))
 
-print(index_min_val)
-
+#Suggeting word for wrong spelling
 print(sim_words[index_min_val])
